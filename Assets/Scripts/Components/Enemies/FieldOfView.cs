ea2a4976e716750c86ced50e3c222ed258c89d03
Ash Dev <69819379+ash-dvlpr@users.noreply.github.com>
2024-04-15 14:23:13 +0200

    B014 - LineOfSight code cleanup and detection modes

--- Assets/Scripts/Components/Enemies/FieldOfView.cs
+++ Assets/Scripts/Components/Enemies/FieldOfView.cs
@@ -7,11 +7,17 @@ using CinderUtils.Extensions;
 
 
 public class FieldOfView : MonoBehaviour {
+    public enum EDetectionMode : byte {
+        STRICT = 0,
+        LOOSE  = 1,
+    }
+
     // ==================== Configuration ====================
     [field: SerializeField, Range(0, 360)] public float ViewAngle { get; private set; }
     [field: SerializeField, Min(0)] public float Range { get; private set; }
     [SerializeField] LayerMask targetMask;
     [SerializeField] LayerMask obstructionMask;
+    [SerializeField] EDetectionMode detectionMode;
 
     // ====================== Variables ======================
     public bool SeenAny => !VisibleTargets.NullOrEmpty();
@@ -47,21 +53,36 @@ public class FieldOfView : MonoBehaviour {
         foreach (Collider targetCollider in targetsInRange) {
             Transform target = targetCollider.transform;
 
-            // Calculate the direction to the target
-            Vector3 displacementToTarget = (target.position - transform.position);
-            Vector3 directionToTarget = displacementToTarget.normalized;
-
-            // If the angle between the target and our direction is less than half the view angle:
-            // Then the target is inside the view cone.
-            if (Vector3.Angle(transform.forward, directionToTarget) < ViewAngle / 2) {
-                // Same as Vector3.Distance(), but doing less operations in this case.
-                float distanceToTarget = displacementToTarget.magnitude;
-
-                // If the LineOfSight is not broken by obstacles, the target is seen.
-                if (!Physics.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionMask)) {
-                    VisibleTargets.Add(target);
-                }
+            // If it's visible, add it to the list.
+            if (TargetIsVisible(target)) {
+                VisibleTargets.Add(target);
             }
         }
+    }
+
+    private bool TargetIsVisible(Transform target) {
+        // Precalculate the displacement (b-a) to the target, which also encodes direction.
+        Vector3 displacementToTarget = (target.position - transform.position);
+
+        switch (detectionMode) {
+            case EDetectionMode.STRICT: return IsInViewCone(displacementToTarget) && HasLineOfSight(displacementToTarget);
+            case EDetectionMode.LOOSE: return IsInViewCone(displacementToTarget);
+            default: return false;
+        }
+    }
+
+    private bool IsInViewCone(Vector3 displacementToTarget) {
+        // If the angle between the target and our direction is less than half the view angle:
+        // Then the target is inside the view cone.
+        float angleToTarget = Vector3.Angle(transform.forward, displacementToTarget.normalized);
+        return angleToTarget < ( ViewAngle / 2 );
+    }
+
+    private bool HasLineOfSight(Vector3 displacementToTarget) {
+        // Same as Vector3.Distance(), but doing less operations in this case.
+        float distanceToTarget = displacementToTarget.magnitude;
+
+        // If the LineOfSight is not broken by obstacles, we have line of sight with the target.
+        return !Physics.Raycast(transform.position, displacementToTarget.normalized, distanceToTarget, obstructionMask);
     }
 }
