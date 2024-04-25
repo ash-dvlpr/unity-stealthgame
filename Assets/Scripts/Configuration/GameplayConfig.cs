ec0e1bf68f6ff96b9e1beebfafc9efdae42738ec
Ash Dev <69819379+ash-dvlpr@users.noreply.github.com>
2024-04-25 14:05:57 +0200

    P008 - Enemy AIPatrol rotations

--- Assets/Scripts/Configuration/GameplayConfig.cs
+++ Assets/Scripts/Configuration/GameplayConfig.cs
@@ -5,9 +5,13 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "CFG_Gameplay", menuName = "Configuration/Gameplay Config", order = 1)]
 public class GameplayConfig : ScriptableObject {
     // ====================== Variables ======================
-    [field: SerializeField] public float MaxTime { get; private set; } = 60f; 
-    [field: SerializeField] public float TimePerObjective { get; private set; } = 20f; 
-    [field: SerializeField] public int MinutesPerDay { get; private set; } = 60; 
+    //[field: Header("General")]
+
+    [field: Header("Time")]
+    [field: SerializeField, Min(0)] public int MinutesPerDay { get; private set; } = 60; 
+    [field: SerializeField, Min(0)] public int MinutesMaxTime { get; private set; } = 1; 
+    [field: SerializeField, Min(0)] public int MinutesPerPatrol { get; private set; } = 5; 
+    [field: SerializeField, Min(0)] public int SecondsPerObjective { get; private set; } = 20;
     
     
     [NonSerialized] int? _secondsPerDay; 
@@ -17,6 +21,26 @@ public class GameplayConfig : ScriptableObject {
                 _secondsPerDay = MinutesPerDay * 60;
             }
             return _secondsPerDay ?? 0;
+        }
+    }
+
+    [NonSerialized] int? _secondsMaxTime; 
+    public int SecondsMaxTime {
+        get {
+            if (_secondsMaxTime == null) {
+                _secondsMaxTime = MinutesMaxTime * 60;
+            }
+            return _secondsMaxTime ?? 0;
+        }
+    }
+
+    [NonSerialized] int? _secondsPerPatrol; 
+    public int SecondsPerPatrol {
+        get {
+            if (_secondsPerPatrol == null) {
+                _secondsPerPatrol = MinutesPerPatrol * 60;
+            }
+            return _secondsPerPatrol ?? 0;
         }
     }
 }
